d95c6bfb3084707dadfe6c392d3deab75e85966a
MANISHKUMAR1999 <panjiyarmanish444@gmail.com>
2024-03-16 22:41:58 +0530

    added hello in app js

--- app.js
+++ app.js
@@ -3,14 +3,16 @@
 
 const names = require("./4-names");
 const sayHi = require("./5-utils");
-const items = require('./6-alternative-flavours')
-const singlePerson = require('./6-alternative-flavours')
+const items = require("./6-alternative-flavours");
+const singlePerson = require("./6-alternative-flavours");
 //console.log(names);
-require('./7-mind-granade')
+require("./7-mind-granade");
 
-sayHi('susan');
+sayHi("susan");
 sayHi(names.john);
-sayHi(names.peter)
+sayHi(names.peter);
+
+console.log("hello");
 
 // console.log(items);
 // console.log(singlePerson);
